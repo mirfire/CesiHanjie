8296ddce4621fa560c30c7c0c5f32f7591c32e1e
R0cknfire <rocknfire@orange.fr>
2014-12-03 15:13:54 +0100

    Test

--- main.c
+++ main.c
@@ -4,13 +4,5 @@
 int main()
 {
     printf("Hello world!\n");
-    printf("Hello world!\n");
-    printf("Hello world!\n");
-    printf("Hello world!\n");
-    printf("Hello world!\n");
-    printf("Hello world!\n");
-    printf("Hello world!\n");
-    printf("Hello world!\n");
-    printf("Hello world!\n");
     return 0;
 }
