e3fc2c692d417a02656f0a25362189dcc4750f6d
R0cknfire <rocknfire@orange.fr>
2014-12-03 15:11:42 +0100

    Ajout projet Code Blocks

--- main.c
+++ main.c
@@ -4,5 +4,13 @@
 int main()
 {
     printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
+    printf("Hello world!\n");
     return 0;
 }
