53ba81713b26e326d8c454dd118b4f86f770bd15
R0cknfire <rocknfire@orange.fr>
2014-12-10 12:15:15 +0100

    Passage en version 0.3

--- menu.c
+++ menu.c
@@ -16,7 +16,7 @@ printf("                  / __\\___ ___(_) /\\  /\\__ _ _ __  (_(_) ___  \n");
 printf("                 / /  / _ / __| |/ /_/ / _` | '_ \\ | | |/ _ \\ \n");
 printf("                / /__|  __\\__ | / __  | (_| | | | || | |  __/ \n");
 printf("                \\____/\\___|___|_\\/ /_/ \\__,_|_| |__/ |_|\\___| \n");
-printf("                                                  |__/       v0.2\n");
+printf("                                                  |__/       v0.3\n");
 printf("\n");
 
 
@@ -24,7 +24,7 @@ printf("\n");
 }
 
 int NomJoueur(char* nomjoueur) {
-    printf("##############################  Cesi Hanjie v0.2  ##############################\n"); // On demande le nom du joueur
+    printf("##############################  Cesi Hanjie v0.3  ##############################\n"); // On demande le nom du joueur
     printf("# Quel est votre nom ?                                                         #\n"); // pour le stocker dans un tableau
     printf("#############################  Entrez votre choix  #############################\n");
     printf("# ");
@@ -41,7 +41,7 @@ int NomJoueur(char* nomjoueur) {
 int MenuPrincipal(DonneesPartie *Partie) { // Menu principal
     int choix = 0;
     SplahScreen();
-    printf("##############################  Cesi Hanjie v0.2  ##############################\n");
+    printf("##############################  Cesi Hanjie v0.3  ##############################\n");
     printf("# Joueur : %s", Partie->nomjoueur);
     printf("# 1) Nouvelle Partie                                                           #\n");
     printf("# 2) Charger Partie                                                            #\n");
@@ -84,7 +84,7 @@ int MenuDifficulte(DonneesPartie* Partie) { // Menu de choix de la difficulté
     Partie->difficulte = 0;
     int choix;
     SplahScreen();
-    printf("##############################  Cesi Hanjie v0.2  ##############################\n");
+    printf("##############################  Cesi Hanjie v0.3  ##############################\n");
     printf("# Choisissez votre difficulté :                                                #\n");
     printf("# 1) Facile                                                                    #\n");
     printf("# 2) Normal                                                                    #\n");
@@ -129,7 +129,7 @@ int MenuGrille(DonneesPartie* Partie) { // Menu de choix des grilles en fonction
     case FACILE:
         EffacerEcran();
         SplahScreen();
-        printf("##############################  Cesi Hanjie v0.2  ##############################\n");
+        printf("##############################  Cesi Hanjie v0.3  ##############################\n");
         printf("# Vous êtes en difficulté facile. Vous avez des grilles de 5x5 cases \n");
         printf("# 1) A\n");
         printf("# 2) Coeur\n");
@@ -161,7 +161,7 @@ int MenuGrille(DonneesPartie* Partie) { // Menu de choix des grilles en fonction
     case MOYEN:
         EffacerEcran();
         SplahScreen();
-        printf("##############################  Cesi Hanjie v0.2  ##############################\n");
+        printf("##############################  Cesi Hanjie v0.3  ##############################\n");
         printf("# Vous êtes en difficulté moyenne. Vous avez des grilles de 10x10 cases \n");
         printf("# 1) Etoile\n");
         printf("# 2) Coeur\n");
@@ -193,7 +193,7 @@ int MenuGrille(DonneesPartie* Partie) { // Menu de choix des grilles en fonction
     case DIFFICILE:
         EffacerEcran();
         SplahScreen();
-        printf("##############################  Cesi Hanjie v0.2  ##############################\n");
+        printf("##############################  Cesi Hanjie v0.3  ##############################\n");
         printf("# Vous êtes en difficulté difficile. Vous avez des grilles de 15x15 cases \n");
         printf("# 1) Champignon\n");
         printf("# 2) Creeper\n");
@@ -230,26 +230,30 @@ int MenuGrille(DonneesPartie* Partie) { // Menu de choix des grilles en fonction
 int MenuSauvegarde(DonneesPartie *Partie) {
     char reponse;
     SplahScreen();
-    printf("##############################  Cesi Hanjie v0.2  ##############################\n"); // Menu de confirmation de chargement de la sauvegarde
+    printf("##############################  Cesi Hanjie v0.3  ##############################\n"); // Menu de confirmation de chargement de la sauvegarde
     printf("# Voulez vraiment charger une partie ?\n");
     printf("# O/N \n");
     printf("#############################  Entrez votre choix  #############################\n");
     printf("# ");
-    scanf("%c", &reponse);
-    ClearBuffer();
-    if((reponse == 79) || (reponse == 111)) {
-        // ChargerPartie();
-        printf("# Ici bientôt, le chargement de la partie.\n");
-        printf("# Retour au menu...\n");
-        sleep(2);
-        EffacerEcran();
-        MenuPrincipal(Partie);
+    if(scanf("%c", &reponse) == 1) {
+        ClearBuffer();
+        if((reponse == 79) || (reponse == 111)) {
+            // ChargerPartie();
+            printf("# Ici bientôt, le chargement de la partie.\n");
+            printf("# Retour au menu...\n");
+            sleep(2);
+            EffacerEcran();
+            MenuPrincipal(Partie);
+        }
+        else {
+            printf("# Retour au menu...\n");
+            sleep(2);
+            EffacerEcran();
+            MenuPrincipal(Partie);
+        }
     }
     else {
-        printf("# Retour au menu...\n");
-        sleep(2);
-        EffacerEcran();
-        MenuPrincipal(Partie);
+        printf("\x1b[31mERREUR\x1b[0m: Entrée invalide.\n");
     }
     return 0;
 }
@@ -258,7 +262,7 @@ int MenuSauvegarde(DonneesPartie *Partie) {
 int MenuHistorique(DonneesPartie* Partie) { // Menu pour afficher l'historique, et lancer le tri de celui ci
     int choix;
     SplahScreen();
-    printf("##############################  Cesi Hanjie v0.2  ##############################\n");
+    printf("##############################  Cesi Hanjie v0.3  ##############################\n");
     printf("# 1) Afficher l'historique                                                     #\n");
     printf("# 2) Afficher l'historique trié par ordre croissant                            #\n");
     printf("# 3) Afficher l'historique trié par ordre décroissant                          #\n");
